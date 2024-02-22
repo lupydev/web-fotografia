5fc2ba01c0123afa74e8680ad747bffbc856590c
Luis Guzman <luis.guzman@i2ds.com.mx>
2024-02-21 20:16:32 -0500

    Se agregan estilos a la pagina web

--- web_fotografia/web_fotografia.py
+++ web_fotografia/web_fotografia.py
@@ -1,11 +1,14 @@
 import reflex as rx
-
+import web_fotografia.styles.style as style
 from .pages.contact import contact
 from .pages.about import about
 from .pages.index import index
 
 
-app = rx.App()
+app = rx.App(
+    stylesheets=style.STYLESHEETS,
+    style=style.BASE_STYLE,
+)
 app.add_page(index, route="/")
 app.add_page(about, route="/sobre-mi")
 app.add_page(contact, route="/contacto")
