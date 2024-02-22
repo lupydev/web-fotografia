e0495dfc955ee3d36f40f80f7cbcb540bf303a91
Luis Guzman <luis.guzman@i2ds.com.mx>
2024-02-21 20:14:00 -0500

    Fuentes

---
+++ web_fotografia/styles/font.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Font(Enum):
+    DEFAULT = "Roboto, Studio-Feixen-Sans, Arial"
+    TITLE = "Roboto, Studio-Feixen-Sans, Arial"
+
+
+class FontWeight(Enum):
+    LIGHT = "300"
+    MEDIUM = "500"
+    BOLD = "700"
