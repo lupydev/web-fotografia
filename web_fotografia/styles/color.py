0e8322c3e9ee9bb1d68eafd785de63615d43005f
Luis Guzman <luis.guzman@i2ds.com.mx>
2024-02-21 20:13:31 -0500

    Color

---
+++ web_fotografia/styles/color.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Color(Enum):
+    PRIMARY = "#A2AD91"
+    SECUNDARY = "#DBB4AD"
+    BACKGROUND = "#FFE3DC"
+
+
+class TextColor(Enum):
+    TITLE = "#FFF"
+    BODY = "#000"
